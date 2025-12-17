38a3dc1de867e699e880d50c5095187be02ab85f
Mithanshu Hedau <144697427+MithanshuHedau@users.noreply.github.com>
2025-12-17 11:28:32 +0530

    Api Updated for docker

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-// const API_BASE_URL = "https://bookmithanshuapi.onrender.com";
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Create axios instance
